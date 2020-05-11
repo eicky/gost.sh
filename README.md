ea15292ab6626813e041da5f29b03095b719d26a
Eicky <eickywl@gmail.com>
2020-05-11 14:16:16 +0800

    fix bugs

--- README.md
+++ README.md
@@ -2,13 +2,26 @@
 ```
 国内使用
 wget https://cdn.jsdelivr.net/gh/eicky/gost.sh/gost.sh && chmod +x gost.sh && bash gost.sh
+
 国外使用
 wget https://git.io/gost.sh && chmod +x gost.sh && bash gost.sh
 ```
 
-说明：目前还是1.0.0版本，仅可以安装最新版本的gost，配置文件在/etc/gost/config.json，运行文件在/usr/bin/gost，进程守护文件在/usr/lib/systemd/system/gost.service
+说明：目前还是1.0.1版本，仅可以安装最新版本的gost，配置文件在`/etc/gost/config.json`，运行文件在`/usr/bin/gost`，进程守护文件在/`usr/lib/systemd/system/gost.service`
 
-使用说明：gost的配置文件需要自己编写，格式参考gost的官方文档，在安装完成后可以通过systemctl start/stop/restart/status gost，来控制gost的启动停止重启状态。要确保wget已经安装，不然无法进行。
+使用说明：gost的配置文件需要自己编写，格式参考gost的[官方文档](https://docs.ginuerzh.xyz/gost/getting-started/)，在安装完成后可以通过`bash gost.sh`来控制gost的启动停止重启状态也可通过`systemctl start/stop/restart/status gost`控制。安装前请确保`wget`已经安装，否则无法安装成功。
+
+端口转发示例：
+```
+{
+    "Debug": true,
+    "Retries": 0,
+    "ServeNodes": [
+           "tcp://:111/1.1.1.1:443",
+           "udp://:222/2.2.2.2:443",
+    ]
+}
+```
 
 
 改自：[https://www.fiisi.com/gost/gost.sh](https://www.fiisi.com/gost/gost.sh)
--- gost.sh
+++ gost.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-sh_ver=1.0.0
+sh_ver=1.0.1
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -23,13 +23,13 @@ check_sys(){
 		release="ubuntu"
 	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
 		release="centos"
-    fi
+  fi
 	bit=$(uname -m)
         if test "$bit" != "x86_64"; then
            echo "请输入你的芯片架构，/386/armv5/armv6/armv7/armv8"
            read bit
         else bit="amd64"
-    fi
+        fi
 }
 
 Installation_dependency(){
@@ -50,10 +50,10 @@ check_root(){
 }
 
 check_new_ver(){
-ct_new_ver=$(wget --no-check-certificate -qO- -t2 -T3 https://api.github.com/repos/ginuerzh/gost/releases/latest| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g;s/v//g')
-if [[ -z ${ct_new_ver} ]]; then
+ct_new_ver=$(wget --no-check-certificate -qO- -t2 -T3 https://gost.eicky.workers.dev | grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g;s/v//g')
+  if [[ -z ${ct_new_ver} ]]; then
 		echo -e "${Error} gost 最新版本获取失败，请手动获取最新版本号[ https://github.com/ginuerzh/gost/releases ]"
-		read -e -p "请输入版本号 [ 格式 x.x.xx , 如 0.8.21 ] :" ct_new_ver
+		read -e -p "请输入版本号 [ 格式 x.x.xx , 如 2.11.0 ] :" ct_new_ver
 		[[ -z "${ct_new_ver}" ]] && echo "取消..." && exit 1
 	else
 		echo -e "${Info} gost 目前最新版本为 ${ct_new_ver}"
@@ -62,26 +62,26 @@ if [[ -z ${ct_new_ver} ]]; then
 check_file(){
             if test ! -d "/usr/lib/systemd/system/";then
              `mkdir /usr/lib/systemd/system`
-            `chmod -R 777 /usr/lib/systemd/system`
-             fi
+             `chmod -R 777 /usr/lib/systemd/system`
+            fi
 }
 check_nor_file(){
            `rm -rf "$(pwd)"/gost`
-             `rm -rf "$(pwd)"/gost.service`
-             `rm -rf "$(pwd)"/config.json`
-             `rm -rf /etc/gost`
-             `rm -rf /usr/lib/systemd/system/gost.service`
-             `rm -rf /usr/bin/gost`
+           `rm -rf "$(pwd)"/gost.service`
+           `rm -rf "$(pwd)"/config.json`
+           `rm -rf /etc/gost`
+           `rm -rf /usr/lib/systemd/system/gost.service`
+           `rm -rf /usr/bin/gost`
 }
 
 Install_ct(){
-           check_root
-           check_nor_file
-           Installation_dependency
-           check_file
-           check_sys
-           check_new_ver
-           `wget --no-check-certificate https://gh.334433.xyz/https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
+            check_root
+            check_nor_file
+            Installation_dependency
+            check_file
+            check_sys
+            check_new_ver
+            `wget --no-check-certificate https://gh.334433.xyz/https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
             `gunzip gost-linux-"$bit"-"$ct_new_ver".gz`
             `mv gost-linux-"$bit"-"$ct_new_ver" gost`
             `mv gost /usr/bin/gost`
@@ -91,17 +91,17 @@ Install_ct(){
             `systemctl enable gost && systemctl restart gost`
             echo "------------------------------"
             if test -a /usr/bin/gost -a /usr/lib/systemctl/gost.service -a /etc/gost/config.json;then
-             echo "${Green_font_prefix} gost似乎安装成功 ${Font_color_suffix}"
+             echo -e "${Green_font_prefix} gost似乎安装成功 ${Font_color_suffix}"
              `rm -rf "$(pwd)"/gost`
              `rm -rf "$(pwd)"/gost.service`
              `rm -rf "$(pwd)"/config.json`
             else
-            echo "${Red_font_prefix} gost没有安装成功，可以提issues反馈 ${Font_color_suffix}"
-             `rm -rf   "$(pwd)"/gost`
+             echo -e "${Red_font_prefix} gost没有安装成功，可以提issues[]反馈 ${Font_color_suffix}"
+             `rm -rf "$(pwd)"/gost`
              `rm -rf "$(pwd)"/gost.service`
              `rm -rf "$(pwd)"/config.json`
              `rm -rf "$(pwd)"/gost.sh`
-             fi
+            fi
 }
 
 Uninstall_ct(){
@@ -109,27 +109,32 @@ Uninstall_ct(){
              `rm -rf /usr/lib/systemd/system/gost.service`
              `rm -rf /etc/gost`
              `rm -rf "$(pwd)"/gost.sh`
-             echo "${Red_font_prefix} gost已经成功删除 ${Font_color_suffix}"
+             echo -e "${Red_font_prefix} gost已经成功删除 ${Font_color_suffix}"
 }
 
 Start_ct(){
           `systemctl start gost`
-          echo "${Green_font_prefix} 已启动 ${Font_color_suffix}"
+          echo -e "${Green_font_prefix} 已启动 ${Font_color_suffix}"
 }
 
 Stop_ct(){
           `systemctl stop gost`
-            echo "${Green_font_prefix} 已停止 ${Font_color_suffix}"  
+          echo -e "${Green_font_prefix} 已停止 ${Font_color_suffix}"  
 }
 
 Restart_ct(){
-            `systemctl restart gost`
-             echo "${Green_font_prefix} 已重启 ${Font_color_suffix}" 
+          `systemctl restart gost`
+          echo -e "${Green_font_prefix} 已重启 ${Font_color_suffix}" 
+}
+
+Status_ct(){
+          echo -e "${Green_font_prefix} 请检查 ${Font_color_suffix}" 
+          echo -e "`systemctl status gost`"
 }
 
 echo "the last version has been launched in my blog" 
-echo && echo -e "  gost 一键安装脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-  ---- eicky | https://eicky.com ----
+echo && echo -e "       Gost一键安装脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+  ---- Eicky | https://eicky.com ----
   
  ${Green_font_prefix}1.${Font_color_suffix} 安装 gost
  ${Green_font_prefix}2.${Font_color_suffix} 卸载 gost
@@ -137,8 +142,9 @@ echo && echo -e "  gost 一键安装脚本 ${Red_font_prefix}[v${sh_ver}]${Font_
  ${Green_font_prefix}3.${Font_color_suffix} 启动 gost
  ${Green_font_prefix}4.${Font_color_suffix} 停止 gost
  ${Green_font_prefix}5.${Font_color_suffix} 重启 gost
+ ${Green_font_prefix}6.${Font_color_suffix} 状态 gost 
 ————————————" && echo
-read -e -p " 请输入数字 [1-5]:" num
+read -e -p " 请输入数字 [1-6]:" num
 case "$num" in
 	1)
 	Install_ct
@@ -155,7 +161,10 @@ case "$num" in
 	5)
 	Restart_ct
 	;;
+  6)
+  Status_ct
+  ;;
 	*)
-	echo "请输入正确数字 [1-5]"
+	echo "请输入正确数字 [1-6]"
 	;;
 esac
